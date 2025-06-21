3a3218678912c592d74db6f5477ccda803373631
Rcloud173 <amirshaikh17543@gmail.com>
2025-06-21 23:52:02 +0530

    latest update

--- P3_Journal App/DB.py
+++ P3_Journal App/DB.py
@@ -5,3 +5,9 @@ class Database:
     def __init__(self):
         self.user_data_file = 'user_data.json'
         self.tasks_data_file = 'tasks_data.json'
+
+    def add_user(self):
+        pass
+
+    def add_journal(self):
+        pass
