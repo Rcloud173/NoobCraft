59c420d7854a76ad8e5f5cbc8c22a9558b98ad5d
Rcloud173 <amirshaikh17543@gmail.com>
2025-12-25 23:54:48 +0530

    new

--- Simulations/MANIM/main.py
+++ Simulations/MANIM/main.py
@@ -20,3 +20,9 @@ class SquareToCircle(Scene):
         self.play(Create(square))  # animate the creation of the square
         self.play(Transform(square, circle))  # interpolate the square into the circle
         self.play(FadeOut(square))  # fade out animation
+
+class CreateCircle(Scene):
+#     def construct(self):
+#         circle = Circle()  # create a circle
+#         circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
+#         self.play(Create(circle))  # show the circle on screen
